2ccb17d41555726d3c59a578b9018e3c2d1a415b
jinsol <72060681+jin123457@users.noreply.github.com>
2022-08-31 22:48:08 +0900

    소셜미디어 로그인 기능 구현

--- src/routes/Auth.js
+++ src/routes/Auth.js
@@ -1,4 +1,6 @@
-import { getAuth, signInWithEmailAndPassword, createUserWithEmailAndPassword } from "firebase/auth";
+import { async } from "@firebase/util";
+import { authService } from "fBase";
+import { getAuth, signInWithEmailAndPassword, createUserWithEmailAndPassword, GoogleAuthProvider, GithubAuthProvider, signInWithPopup } from "firebase/auth";
 import React, { useState } from "react";
 
 const Auth = () => {
@@ -32,6 +34,18 @@ const Auth = () => {
         }
     };
     const toggleAccount = () => setNewAccount((prev) => !prev);
+    const onSocialClick = async (e) => {
+        const {
+            target: { name },
+        } = e;
+        let provider;
+        if (name === "google") {
+            provider = new GoogleAuthProvider();
+        } else if (name === "github") {
+            provider = new GithubAuthProvider();
+        }
+        await signInWithPopup(authService, provider);
+    };
     return (
         <div>
             <form onSubmit={onSubmit}>
@@ -42,8 +56,12 @@ const Auth = () => {
             {error}
             <span onClick={toggleAccount}>{newAccount ? "Sign In" : "Create Account"}</span>
             <div>
-                <button>Continue With Google</button>
-                <button>Continue With Github</button>
+                <button name="google" onClick={onSocialClick}>
+                    Continue With Google
+                </button>
+                <button name="github" onClick={onSocialClick}>
+                    Continue With Github
+                </button>
             </div>
         </div>
     );
