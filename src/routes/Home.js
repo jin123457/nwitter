e9b82335729655bba6389d18a16b6bc32ae5a1ff
jinsol <72060681+jin123457@users.noreply.github.com>
2022-09-01 18:14:42 +0900

    firebase 컬렉션 생성 및 도큐멘트 생성

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,21 +1,35 @@
+import { dbSerive } from "fBase";
+import { addDoc, collection } from "firebase/firestore";
 import React, { useState } from "react";
 
-const Home =  () => {
-    const [nweet,setNweet] = useState('');
-    const onSubmit = (e) => {
+const Home = () => {
+    const [nweet, setNweet] = useState("");
+    const onSubmit = async (e) => {
         e.preventDefault();
-    }
+        try {
+            const docRef = await addDoc(collection(dbSerive, "Nweets"), {
+                nweet,
+                createAt: Date.now(),
+            });
+            console.log(docRef);
+        } catch (error) {
+            console.log(error.message);
+        }
+        setNweet("");
+    };
     const onChange = (e) => {
-        const {target : {value}} = e;
+        const {
+            target: { value },
+        } = e;
         setNweet(value);
-    }
+    };
     return (
         <div>
             <form onSubmit={onSubmit}>
-                <input value={nweet} type="text" maxLength="120" placeholder="what's on your mind?" onChange={onChange}/>
+                <input value={nweet} type="text" maxLength="120" placeholder="what's on your mind?" onChange={onChange} />
                 <input type="submit" value="Nweet" />
             </form>
         </div>
     );
-}
+};
 export default Home;
