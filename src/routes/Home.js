e9cae7f574a62570e1674ce994d3f6ed517faff7
jinsol <72060681+jin123457@users.noreply.github.com>
2022-09-01 21:32:33 +0900

    데이터 베이스에 있는 데이터 출력

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,9 +1,24 @@
 import { dbSerive } from "fBase";
-import { addDoc, collection } from "firebase/firestore";
-import React, { useState } from "react";
+import { addDoc, collection, query, getDocs } from "firebase/firestore";
+import React, { useEffect, useState } from "react";
 
 const Home = () => {
     const [nweet, setNweet] = useState("");
+    const [nweets, setNweets] = useState([]);
+    const getNweets = async () => {
+        const dbnweets = query(collection(dbSerive, "Nweets"));
+        const querySnapshot = await getDocs(dbnweets);
+        querySnapshot.forEach((doc) => {
+            const nweetObj = {
+                ...doc.data(),
+                id: doc.id,
+            };
+            setNweets((prev) => [nweetObj, ...prev]);
+        });
+    };
+    useEffect(() => {
+        getNweets();
+    }, []);
     const onSubmit = async (e) => {
         e.preventDefault();
         try {
@@ -11,7 +26,6 @@ const Home = () => {
                 nweet,
                 createAt: Date.now(),
             });
-            console.log(docRef);
         } catch (error) {
             console.log(error.message);
         }
@@ -23,12 +37,20 @@ const Home = () => {
         } = e;
         setNweet(value);
     };
+    console.log(nweets);
     return (
         <div>
             <form onSubmit={onSubmit}>
                 <input value={nweet} type="text" maxLength="120" placeholder="what's on your mind?" onChange={onChange} />
                 <input type="submit" value="Nweet" />
             </form>
+            <div>
+                {nweets.map((nweet) => (
+                    <div key={nweet.id}>
+                        <h4>{nweet.nweet}</h4>
+                    </div>
+                ))}
+            </div>
         </div>
     );
 };
