70123251589e06520b15fe01bc08f03e4e676bf1
jxxjx__ <72060681+jin123457@users.noreply.github.com>
2022-09-01 16:56:34 +0900

    firebase 데이터베이스 셋팅

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,4 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 
-const Home =  () => <span>Home</span>;
+const Home =  () => {
+    const [nweet,setNweet] = useState('');
+    const onSubmit = (e) => {
+        e.preventDefault();
+    }
+    const onChange = (e) => {
+        const {target : {value}} = e;
+        setNweet(value);
+    }
+    return (
+        <div>
+            <form onSubmit={onSubmit}>
+                <input value={nweet} type="text" maxLength="120" placeholder="what's on your mind?" onChange={onChange}/>
+                <input type="submit" value="Nweet" />
+            </form>
+        </div>
+    );
+}
 export default Home;
