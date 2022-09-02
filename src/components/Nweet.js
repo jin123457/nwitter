a39a88496f993829b26136ba1add0ba00b867287
jxxjx__ <jin_ox@naver.com>
2022-09-03 01:38:40 +0900

    [기능 추가] 트위팅 수정 및 삭제 기능 추가

---
+++ src/components/Nweet.js
@@ -0,0 +1,62 @@
+import { async } from "@firebase/util";
+import { dbSerive } from "fBase";
+import { doc, deleteDoc, updateDoc }from"firebase/firestore";
+import React, { useState } from "react";
+
+
+const Nweet = ({nweetObj,isOwner}) => {
+    const [editing,setEditing] = useState(false);
+    const [newNweet,setNewNweet] = useState(nweetObj.text);
+    const NweetTextRef = doc(dbSerive, "Nweets", `${nweetObj.id}`);
+    const onDeleteClick = () => {
+        const ok = window.confirm("Are you sure you want to delete this nweet?");
+        if(ok){
+            // delete 부분
+            deleteDoc(NweetTextRef);
+        }
+    }
+    const toggleEditing = () => setEditing((prev) => !prev);
+    const onSubmit = async(e) => {
+        e.preventDefault();
+        //update 부분
+        await updateDoc(NweetTextRef, {
+            text: newNweet,
+        });
+        setEditing(false);
+        
+    }
+    const onChange = (e) => {
+        const {target : {value},} = e;
+        setNewNweet(value);
+    }
+    return (
+        <div>
+            {
+                editing ? (
+                    <>
+                        <form onSubmit={onSubmit}>
+                            <input type="text" placeholder="Edit your nweet?" value={newNweet} onChange={onChange}/>
+                            <input value="update nweet" type="submit" />
+                        </form>
+                        <button onClick={toggleEditing}>cancle</button>
+                    </>
+                ) : (
+                    <>
+                        <h4>{nweetObj.text}</h4>
+                        {
+                        isOwner && 
+                            (
+                                <>
+                                    <button onClick={onDeleteClick}>Delete nweet</button>
+                                    <button onClick={toggleEditing}>Edit nweet</button>
+                                </>
+                            )
+                        }
+                    </>
+                )
+            }
+        </div>
+    );
+}
+
+export default Nweet;
--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,3 +1,4 @@
+import Nweet from "components/Nweet";
 import { dbSerive } from "fBase";
 import { addDoc, collection, onSnapshot, orderBy, query } from "firebase/firestore";
 import React, { useEffect, useState } from "react";
@@ -13,7 +14,6 @@ const Home = ({ userObj }) => {
                 ...document.data(),
             }));
             setNweets(nweetArr);
-            console.log(nweetArr);
         });
     }, []);
     const onSubmit = async (e) => {
@@ -45,9 +45,7 @@ const Home = ({ userObj }) => {
             </form>
             <div>
                 {nweets.map((nweet) => (
-                    <div key={nweet.id}>
-                        <h4>{nweet.text}</h4>
-                    </div>
+                    <Nweet key={nweet.id} nweetObj={nweet} isOwner={nweet.creatorId === userObj.uid} />
                 ))}
             </div>
         </div>
